ad809228832fd1761f803dd9b179e0e8aa414d8d
Danlin Luo <danlinluo432@gmail.com>
2024-01-20 16:15:50 -0500

    Change data type back to datetime

--- backend/data_parse.py
+++ backend/data_parse.py
@@ -1,9 +1,7 @@
 import pandas as pd
 
 request_date = "Request Date"
-request_time = "Request Time"
 appt_date = "Appointment Date"
-appt_time = "Appointment Time"
 vehicle_type = "Vehicle Type"
 walk_in = "Walk-In"
 
@@ -16,10 +14,4 @@ df[request_date] = pd.to_datetime(df[request_date])
 df[appt_date] = pd.to_datetime(df[appt_date])
 df.sort_values(by=[appt_date, request_date], inplace=True)
 
-df[request_date] = df[request_date].dt.strftime("%Y-%m-%d %X")
-df[appt_date] = df[appt_date].dt.strftime("%Y-%m-%d %X")
-
-df[[request_date, request_time]] = df[request_date].str.split(" ", n=1, expand=True)
-df[[appt_date, appt_time]] = df[appt_date].str.split(" ", n=1, expand=True)
-
-df = df[[appt_date, appt_time, request_date, request_time, vehicle_type, walk_in]]
+df = df[[appt_date, request_date, vehicle_type, walk_in]]
